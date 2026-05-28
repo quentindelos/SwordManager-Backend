66d7b36abb1a54dba6e7483e0147b83d2ae9df56
Quentin Delos <90725340+quentindelos@users.noreply.github.com>
2026-05-28 11:00:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const { sequelize } = require('./models');
 
 const app = express();
 app.use(cors({
+    // On autorise localhost ET l'IP 127.0.0.1
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://www.swordmanager.cloud', 'https://swordmanager.cloud'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -15,24 +16,18 @@ app.use(cors({
 }));
 app.use(express.json());
 
+// Routes
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/vault', require('./routes/vaultRoutes'));
 
 const start = async () => {
   try {
-    await sequelize.authenticate();
-    console.log('Connexion à la base de données réussie.');
-    
     await sequelize.sync();
-    
     const port = process.env.PORT || 8080;
     app.listen(port, '0.0.0.0', () => {
-      console.log(`Serveur prêt sur le port ${port}`);
+      console.log(`🚀 Serveur prêt sur le port ${port}`);
     });
-  } catch (e) { 
-    console.error('Erreur critique de base de données:', e);
-    process.exit(1); 
-  }
+  } catch (e) { console.error(e); }
 };
 
 start();
